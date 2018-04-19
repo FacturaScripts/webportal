b2bc0d9964991b35c3419ff00785e75adaa8e920
NeoRazorX <neorazorx@gmail.com>
2018-04-20 00:29:24 +0200

    Added Permalink class and minor changes.

---
+++ Lib/WebPortal/Permalink.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * This file is part of webportal plugin for FacturaScripts.
+ * Copyright (C) 2018  Carlos Garcia Gomez  <carlos@facturascripts.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace FacturaScripts\Plugins\webportal\Lib\WebPortal;
+
+/**
+ * Description of Permalink
+ *
+ * @author Carlos García Gómez
+ */
+class Permalink
+{
+
+    public static function get(string $title, int $length = 200)
+    {
+        $changes = array('/à/' => 'a', '/á/' => 'a', '/â/' => 'a', '/ã/' => 'a', '/ä/' => 'a',
+            '/å/' => 'a', '/æ/' => 'ae', '/ç/' => 'c', '/è/' => 'e', '/é/' => 'e', '/ê/' => 'e',
+            '/ë/' => 'e', '/ì/' => 'i', '/í/' => 'i', '/î/' => 'i', '/ï/' => 'i', '/ð/' => 'd',
+            '/ñ/' => 'n', '/ò/' => 'o', '/ó/' => 'o', '/ô/' => 'o', '/õ/' => 'o', '/ö/' => 'o',
+            '/ő/' => 'o', '/ø/' => 'o', '/ù/' => 'u', '/ú/' => 'u', '/û/' => 'u', '/ü/' => 'u',
+            '/ű/' => 'u', '/ý/' => 'y', '/þ/' => 'th', '/ÿ/' => 'y',
+            '/&quot;/' => '-'
+        );
+        $permalink = preg_replace(array_keys($changes), $changes, strtolower($title));
+        $permalink = preg_replace('/[^a-z0-9]/i', '-', $permalink);
+        $permalink = preg_replace('/-+/', '-', $permalink);
+
+        if (substr($permalink, 0, 1) == '-') {
+            $permalink = substr($permalink, 1);
+        }
+
+        if (substr($permalink, -1) == '-') {
+            $permalink = substr($permalink, 0, -1);
+        }
+
+        if (strlen($permalink) > $length) {
+            return substr($permalink, 0, $length - 1);
+        }
+
+        return $permalink;
+    }
+}
