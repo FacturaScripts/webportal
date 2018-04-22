fef122ba3c3debb074bbe7ebd5fc187e598afde2
NeoRazorX <neorazorx@gmail.com>
2018-04-22 11:52:55 +0200

    Removed unnecesary spaces.

--- Controller/HybridLogin.php
+++ Controller/HybridLogin.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of webportal plugin for FacturaScripts.
- * Copyright (C) 2018  Carlos Garcia Gomez  <carlos@facturascripts.com>
+ * Copyright (C) 2018 Carlos Garcia Gomez  <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -14,7 +14,7 @@
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 namespace FacturaScripts\Plugins\webportal\Controller;
 
@@ -70,7 +70,7 @@ class HybridLogin extends PortalController
             case 'facebook':
                 $this->facebookLogin();
                 break;
-            
+
             case 'google':
                 $this->googleLogin();
                 break;
@@ -78,7 +78,7 @@ class HybridLogin extends PortalController
             case 'twitter':
                 $this->twitterLogin();
                 break;
-            
+
             default:
                 $this->miniLog->alert('no-login-provider');
                 break;
@@ -130,7 +130,7 @@ class HybridLogin extends PortalController
             $this->miniLog->error($exc->getMessage());
         }
     }
-    
+
     private function googleLogin()
     {
         $config = [
