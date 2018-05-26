ce2b50404234fb906d6f9b519241277c97b56746
NeoRazorX <neorazorx@gmail.com>
2018-05-26 18:08:45 +0200

    Added page filter to block tab in ListWebPages.

--- Controller/ListWebPage.php
+++ Controller/ListWebPage.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of webportal plugin for FacturaScripts.
- * Copyright (C) 2018 Carlos Garcia Gomez  <carlos@facturascripts.com>
+ * Copyright (C) 2018 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -84,6 +84,9 @@ class ListWebPage extends ExtendedController\ListController
 
         $blockTypes = $this->codeModel->all('webblocks', 'type', 'type');
         $this->addFilterSelect('ListWebBlock', 'type', 'type', 'type', $blockTypes);
+
+        $pages = $this->codeModel->all('webpages', 'idpage', 'permalink');
+        $this->addFilterSelect('ListWebBlock', 'idpage', 'page', 'idpage', $pages);
     }
 
     protected function createViewWebCluster()
