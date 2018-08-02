9f4c1360dba14e88e84f85c125a2bdc8dc917ea9
Cristo Estévez <cristom.estevez@gmail.com>
2018-08-02 15:11:38 +0100

    Added method to changed password and changed personal data (address, zip code, etc)

--- Controller/EditProfile.php
+++ Controller/EditProfile.php
@@ -41,17 +41,53 @@ class EditProfile extends SectionController
     protected function execPreviousAction(string $action)
     {
         if ($action === 'edit') {
-            $this->contact->nombre = $this->request->get('nombre', '');
-            $this->contact->apellidos = $this->request->get('apellidos', '');
-            if ($this->contact->save()) {
-                $this->miniLog->notice($this->i18n->trans('record-updated-correctly'));
-            } else {
-                $this->miniLog->alert($this->i18n->trans('record-save-error'));
+            $this->changedPersonalData();
+
+            if($this->changedPassword()) {
+                if ($this->contact->save()) {
+                    $this->miniLog->notice($this->i18n->trans('record-updated-correctly'));
+                } else {
+                    $this->miniLog->alert($this->i18n->trans('record-save-error'));
+                }
             }
             return true;
         }
 
         return parent::execPreviousAction($action);
+    }
+
+    private function changedPassword() : bool
+    {
+        $password = $this->request->get('password', '');
+        $repassword = $this->request->get('re-password', '');
+        
+        if ('' !== $password && $repassword !== '') {
+            return true;
+        }
+
+        if ($password !== $repassword) {
+            $this->miniLog->error($this->i18n->trans('different-passwords-to-contact-' . $this->contact->nombre));
+            return false;
+        }
+
+        if (!$this->contact->verifyPassword($password)) {
+            $this->miniLog->error($this->i18n->trans('not-can-update-password'));
+            return false;
+        }
+
+        return true;
+    }
+
+    private function changedPersonalData()
+    {
+        $this->contact->nombre = $this->request->get('nombre', '');
+        $this->contact->apellidos = $this->request->get('apellidos', '');
+        $this->contact->direccion = $this->request->get('direccion', '');
+        $this->contact->apartado = $this->request->get('apartado', '');
+        $this->contact->codpostal = $this->request->get('codpostal', '');
+        $this->contact->ciudad = $this->request->get('ciudad', '');
+        $this->contact->provincia = $this->request->get('provincia', '');
+        $this->contact->codpais = $this->request->get('codpais', '');
     }
 
     protected function loadData(string $sectionName)
