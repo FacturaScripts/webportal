5e1168df180e9a94f024d857b8f800265e9628c6
NeoRazorX <neorazorx@gmail.com>
2019-04-08 20:39:17 +0200

    Minor fix.

--- Lib/WebPortal/SectionController.php
+++ Lib/WebPortal/SectionController.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of webportal plugin for FacturaScripts.
- * Copyright (C) 2018 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2018-2019 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -19,8 +19,8 @@
 namespace FacturaScripts\Plugins\webportal\Lib\WebPortal;
 
 use FacturaScripts\Core\Model\CodeModel;
-use FacturaScripts\Core\Lib\AssetManager;
-use FacturaScripts\Core\Lib\Widget\VisualItem;
+use FacturaScripts\Dinamic\Lib\AssetManager;
+use FacturaScripts\Dinamic\Lib\Widget\VisualItem;
 use FacturaScripts\Plugins\webportal\Lib\WebPortal\Widget\WidgetAutocomplete;
 
 /**
