9f64ac5901430b4b660da9e2c4661007dc1e815a
NeoRazorX <neorazorx@gmail.com>
2019-05-04 13:16:35 +0200

    Removed default text-justify class to markdown paragraphs.

--- Lib/WebPortal/Widget/Markdown.php
+++ Lib/WebPortal/Widget/Markdown.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file is part of webportal plugin for FacturaScripts.
- * Copyright (C) 2018 Carlos Garcia Gomez <carlos@facturascripts.com>
+ * Copyright (C) 2018-2019 Carlos Garcia Gomez <carlos@facturascripts.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -42,7 +42,7 @@ class Markdown
 
         /// some html fixes
         return str_replace(
-            ['<p>', '<pre>', '<img '], ['<p class="text-justify">', '<pre class="code">', '<img class="img-responsive" '], $html
+            ['<pre>', '<img '], ['<pre class="code">', '<img class="img-responsive" '], $html
         );
     }
 }
